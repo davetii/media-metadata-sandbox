f493efbfd1c7dd39bacc950b80a88194b2a6973c
Dave Turner <davetii@gmail.com>
2020-01-26 16:04:17 -0500

    Cleaning up image layout, code, adding readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # media-metadata-sandbox
+
+## Todo
+* Size the Metadata image smaller
+* support for changing Metadata
+* Toggle between readonly and editable
+* Add test libraries
+* Introduce macro mode
+* Column level changes in macro mode
+
+
--- index.js
+++ index.js
@@ -13,13 +13,11 @@ app.on('ready', () => {
 });
 
 eventEmitter.on('load-file', (files) => {
-    console.log('load-file');
-    console.log(files);
     mm.parseFile(files[0])
         .then( metadata => {
             metadata.common.fileName = files[0];
             if (metadata.common.picture) {
-                let base64data =metadata.common.picture[0].data.toString('base64');
+                const base64data =metadata.common.picture[0].data.toString('base64');
                 metadata.common.imageContent = 'data:image/png;base64, ' +  base64data;
             }
             mainWindow.webContents.send('populate-form' , metadata.common);
