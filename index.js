87080fb433d5df265a451dcc7b8219a7f7488940
Dave Turner <davetii@gmail.com>
2020-01-26 15:52:53 -0500

    Support for displaying image

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ eventEmitter.on('load-file', (files) => {
     console.log(files);
     mm.parseFile(files[0])
         .then( metadata => {
-            console.log(util.inspect(metadata.common));
             metadata.common.fileName = files[0];
+            if (metadata.common.picture) {
+                let base64data =metadata.common.picture[0].data.toString('base64');
+                metadata.common.imageContent = 'data:image/png;base64, ' +  base64data;
+            }
             mainWindow.webContents.send('populate-form' , metadata.common);
         })
         .catch( err => {
