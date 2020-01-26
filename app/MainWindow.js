395134980076c0a109cb209279a4cbc3587f15ae
Dave Turner <davetii@gmail.com>
2020-01-26 01:10:53 -0500

    Reading Metadata

--- app/MainWindow.js
+++ app/MainWindow.js
@@ -1,5 +1,7 @@
 const electron = require('electron');
 const { app,BrowserWindow } = electron;
+const mm = require('music-metadata');
+const util = require('util');
 
 class MainWindow extends BrowserWindow {
 
@@ -14,10 +16,27 @@ class MainWindow extends BrowserWindow {
 
 
 
-    loadFile(file) {
+    loadFile(files) {
         if (!this.isOnItem) {
             this.loadURL(`file://${__dirname}/item.html`);
             this.isOnItem = true;
+
+
+            mm.parseFile(files[0])
+                .then( metadata => {
+                    console.log(util.inspect(metadata, { showHidden: false, depth: null }));
+                    //console.log(util.inspect(metadata.common.picture));
+                    // let base64Data = metadata.common.picture[0].data;
+                    // console.log(metadata.common.picture[0].data);
+
+
+                    // require("fs").writeFile("out.png", base64Data, 'base64', function(err) {
+                    //    console.log(err);
+                    //});
+                })
+                .catch( err => {
+                    console.error(err.message);
+                });
         }
     }
 }
